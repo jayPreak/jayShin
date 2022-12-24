7b20ebbf88149f1b2d6842e10a899218dc63f170
jayPreak <jayesh.preak2003@gmail.com>
2022-12-25 00:04:16 +0530

    bruh

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import './styles.css';
+// import './styles.css';
+import './style.css'
 import Comingsoon from './components/Comingsoon';
 import Logo from './Logo';
 import rocket from './assets/rocket1.png'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="container">
-      <Logo />
-      {/* <h1>Coming Soon</h1> */}
-      <Comingsoon />
-      <img src={rocket} className="rocket" alt="Rocket" id="rocket" />
+    
+    <div>
+      <Navbar />
     </div>
+    // <div className="container">
+    //   <Logo />
+    //   <Comingsoon />
+    //   <img src={rocket} className="rocket" alt="Rocket" id="rocket" />
+    // </div>
   );
 }
 
